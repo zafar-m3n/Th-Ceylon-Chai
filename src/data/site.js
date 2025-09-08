c0ccfe63a3be38518cfa8403d9b53fd8e07722b5
zafar-m3n <thezafar.m3n@gmail.com>
2025-09-08 17:34:41 +0530

    Email

--- src/data/site.js
+++ src/data/site.js
@@ -2,7 +2,7 @@ export const siteData = {
   brandName: "T&H Ceylon Chai",
   brandTagline: "Ceylon heritage in every cup",
   whatsappPhone: "+94 77 749 0000",
-  email: "hakeem@thceylonchai.com",
+  email: "newcolomboroyal@gmail.com",
   location: "Colombo, Sri Lanka",
   socials: {
     instagram: "https://www.instagram.com/teaandherbs.sl",
